d991cf99f0361aa64a81790efaa5dc914ce27045
Craig McGowan <craigmcgowan@Craigs-MacBook-Pro.local>
2021-11-05 21:08:07 -0400

    frontend complete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import "./App.css";
+
+// IMPORT COMPONENTS
+import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Header from "./components/Header"
-import Home from "./pages/Home"
+import { Route, Switch } from "react-router-dom";
+// IMPORT PAGES
+import Home from "./pages/Home";
 import About from "./pages/About";
 import Projects from "./pages/Projects";
-import { Route, Switch } from "react-router";
 
 function App() {
-  const URL = "https://react-express-lab.herokuapp.com/";
+  // URL should have YOUR HEROKU URL for your backend, make sure you include the trailing slash
+  const URL = "http://localhost:4000/";
+
   return (
     <div className="App">
       <Header />
@@ -25,4 +31,5 @@ function App() {
     </div>
   );
 }
+
 export default App;
